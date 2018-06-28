4659bb89d24b917559598f087ac6fc428b4098c2
Eric <17600100962@163.com>
2018-06-28 15:30:56 +0800

    sdfsdfsdfsd Signed-off-by: Eric <17600100962@163.com>

---
+++ vue01/src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+      {{ hello }}
+      {{ 1+1 }}
+      {{ "哈哈" }}
+      {{0>10?"对":"错"}}
+      <p> {{ flag }} </p>
+      <div v-html="flag"></div>
+      <p v-text="hello"></p>
+      <p v-html="xhtml"></p>
+      <P> {{ xhtml }} </P>
+      <span>哈哈哈</span>
+      <p>{{ message.split('').reverse().join('') }}</p>
+      <p v-if="ok">YES</p>
+      <p v-else>NO</p>
+      <div class="list">
+          <ul>
+            <!-- <li v-for="name in names">{{ name }}</li> -->
+            <li v-for="name in names" >{{ name }}</li>
+          </ul>
+          <ul>
+            <!-- <li v-for="name in names">{{ name }}</li> -->
+            <li v-for="(name,index) in names" v-bind:key="index">{{ name }}---{{ index }}</li>
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      hello: "hello VUE",
+      flag: "我是flag",
+      xhtml: "<span>这是一个span</span>",
+      message: "Eric",
+      ok: false,
+      names: ["allen", "tomas", "乔治"]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
